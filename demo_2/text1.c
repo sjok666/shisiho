3fc269e444385246d45cc994566efa90a4cad573
sjok666 <3102861440@qq.com>
2022-05-07 22:38:16 +0800

    5-7

---
+++ demo_2/text1.c
@@ -0,0 +1,167 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a','b','c',0 };
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+
+
+	/*char arr1[] = "abc";
+	char arr2[] = { 'a','b','c',0 };
+	printf("%s\n", arr1);
+	printf("%s\n", arr2);*/
+
+
+	/*int num1 = 0;
+	int num2 = 0;
+	int sum = 0;
+	scanf_s("%d%d", &num1, &num2);
+	
+	sum = num1 + num2;
+	printf("sum = %d\n", sum);*/
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//	printf("%d\n", strlen("c:\text\32\text.c"));
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int input = 0;
+//	printf("你要好好学习吗？(1/0)>:");
+//	scanf_s("%d\n", &input);
+//	if (input == 1)
+//		printf("好offer\n");
+//	else
+//		printf("maihongshu\n");
+//	return 0;
+//int main()
+//{
+//	int line = 0;
+//	printf("加入比特\n");
+//	while (line < 2000)
+//	{
+//
+//	printf("齐奥一行代码\n");
+//	line++;
+//	}
+//	
+//	if (line >= 2000)
+//			printf("好offer\n");
+//}
+//
+//Add(int x ,int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int a = 100;
+//	int b = 200;
+//	int sum = 0;
+//	sum = Add(a, b);
+//	printf("sum = %d\n", sum);
+//}
+//#include <stdio.h>
+//void main()
+//{
+//	int a = 1, b = 2, sum;
+//	sum = a + b;
+//	printf("机械2103邵杰，a+b=%d\n", sum);
+//}
+
+//Max(int x, int y)
+//
+//{
+//	if (x > y)
+//		return (x);
+//	else
+//		return(y);
+//}
+//
+//
+//
+//
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 10, b = 20, max;
+//	max = Max(a, b);
+//	printf("max=%d\n", max);
+//
+//
+//}
+//int main()
+//{
+//	int a = 0, b = ~a;
+//	printf("%d\n", b);
+//}
+//Max(int x, int y)
+//
+//{
+//	if (x > y)
+//		return (x);
+//	else
+//		return(y)
+//}
+//
+//
+//
+//
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 10, b = 20, max;
+//	max = Max(a, b);
+//	printf("max=%d\n", max);
+//
+//
+//}
+//#include <stdio.h>
+//void main()
+//{
+//	char c1 = 97, c2 = 98;
+//	printf("%d,%c", c1, c2 + 1);
+//}
+//void text()
+//{
+//	static int a = 1;
+//	a++;
+//	printf("a=%d\n",a);
+//
+//}
+//
+//
+//int main()
+//{
+//	int i = 0;
+//	while (i < 5)
+//	{
+//		text();
+//		i++;
+//
+//	}
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int* p = &a;
+//	printf("%d\n", sizeof(p));
+//}
+//int main()
+//{
+//	int a = 0;
+//	double i = 5.6;
+//	a = (int)i;
+//	printf("%d\n", a);
+//	printf("%f\n", i);
+//}
+
