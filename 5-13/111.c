ac477a692d6d87bb0604062e3d2d60a99bbdfcf9
sjok666 <3102861440@qq.com>
2022-05-13 23:31:04 +0800

    5-13

---
+++ 5-13/111.c
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0,j = 0;
+//	int arr[20] = { 0 };
+//	for (i = 0; i <= 20; i++)
+//	{
+//		if (i % 2 == 0)
+//			arr[j++] = i;
+//
+//	}
+//	for (i = 0; i < j; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//}
---
+++ 5-13/5-13.c
@@ -0,0 +1,20 @@
+//#include <stdio.h>
+//int main()
+//{
+//	static char a[3];
+//	scanf_s("%s", a,3);
+//	printf("%c%c", a[1], a[2]);
+//}
+//int main()
+//{
+//	char arr[] = "tianjin";
+//	int sz = sizeof(arr);
+//	printf("%d", sz);
+//}
+//int main()
+//{
+//	int i;
+//	int x[3][3] = { 9,8,7,6,5,4,3,2,1 };
+//	for (i = 0; i < 3; i++)
+//		printf("%5d", x[1][i]);
+//}
